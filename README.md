6365cc2a2d9fff86e36cd5443a0af1bc21e708af
ToyasDhake <toyasdhake@gmail.com>
2019-11-18 11:39:38 -0800

    Added cpplint and cppcheck results

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # roomba
+https://drive.google.com/file/d/1FcQJpExevW2-YiA0dzui91atSanxLAgw/view?usp=sharing
--- include/Distance.hpp
+++ include/Distance.hpp
@@ -1,6 +1,31 @@
-
-#ifndef INCLUDE_DEPTHCALCULATION_HPP_
-#define INCLUDE_DEPTHCALCULATION_HPP_
+/*
+* Copyright [MIT] 2019 Toyas Dhake
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* @file Distance.hpp
+* @author Toyas Dhake
+* @date 18 Nov 2019
+* @copyright 2019 Toyas Dhake
+* @brief Declaration of class DistanceCalcualtion to calculate distance
+*/
+#ifndef INCLUDE_DISTANCE_HPP_
+#define INCLUDE_DISTANCE_HPP_
 
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
@@ -10,18 +35,17 @@
 
 
 
-class DepthCalculation {
+class DistanceCalculation {
  private:
   bool collision = false;
 
  public:
-  DepthCalculation();
-  ~DepthCalculation();
-  bool Collision();
+  DistanceCalculation();
+  ~DistanceCalculation();
+  bool isCollision();
   void findDepth(const sensor_msgs::LaserScan::ConstPtr& msg);
-  
 };
 
-#endif  // INCLUDE_DEPTHCALCULATION_HPP_
+#endif  // INCLUDE_DISTANCE_HPP_
 
 
